3c0e391ca84501dfd867dd035d5c38e8903bf859
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-03-19 00:02:36 -0300

    Challenge 2 - with description

--- Challenge2.js
+++ Challenge2.js
@@ -1,3 +1,11 @@
+// Crie um hashmap (ou um objeto simples em JavaScript) que contenha as funções
+// elementares de adição, subtração, multiplicação e divisão.
+// // a. Leve em consideração que a divisão não deve permitir dividendo 0
+// // b. A função sum permite um array como parâmetro de entrada e adiciona todos os seus elementos.
+// // c. A função de subtração permite um array como parâmetro de entrada e subtrai todos os seus elementos.
+// // d. Função de multiplicação - Idem
+// // e. A função de divisão aceita dois parâmetros: a e b.
+
 const onErrorMsg = "You can't divide by zero"
 
 const op = {
